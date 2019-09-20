2a9baed72ad1d8473cf3edeb90eb2a314c83b4fd
Bartek <bartoszlis87@gmail.com>
2019-09-20 13:46:18 +0200

    Task1

---
+++ src/pl/coderslab/Task1.java
@@ -0,0 +1,32 @@
+/*
+POWRÓT
+Warsztaty I Java
+v3.2
+Jak zacząć?
+Następnie wykonaj commit zmian komendą:
+git commit -m "nazwa_commita"
+Wypchnij zmiany do swojego repozytorium na GitHubie. Użyj do tego komendy
+git push origin master
+Zadania
+Gra w zgadywanie liczb
+Symulator LOTTO
+Gra w zgadywanie liczb 2
+Kostka do gry
+Wyszukiwarka najpopularniejszych słów
+Gra w zgadywanie liczb
+Napisz prostą grę w zgadywanie liczb. Komputer ma wylosować liczbę w zakresie od 1 do 100. Następnie:
+
+wypisać: "Zgadnij liczbę" i pobrać liczbę z klawiatury;
+sprawdzić, czy wprowadzony napis, to rzeczywiście liczba i w razie błędu wyświetlić komunikat: "To nie jest liczba", po czym wrócić do pkt. 1;
+jeśli liczba podana przez użytkownika jest mniejsza niż wylosowana, wyświetlić komunikat: "Za mało!", po czym wrócić do pkt. 1;
+jeśli liczba podana przez użytkownika jest większa niż wylosowana, wyświetlić komunikat: "Za dużo!", po czym wrócić do pkt. 1;
+jeśli liczba podana przez użytkownika jest równa wylosowanej, wyświetlić komunikat: "Zgadłeś!", po czym zakończyć działanie programu.
+ */
+
+package pl.coderslab;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+    }
+}
