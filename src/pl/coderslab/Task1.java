5d4a6b4a6c6bf9115827b6422c9c1c53536a491c
Bartek <bartoszlis87@gmail.com>
2019-09-20 15:16:55 +0200

    Task_1

--- src/pl/coderslab/Task1.java
+++ src/pl/coderslab/Task1.java
@@ -25,8 +25,55 @@ jeśli liczba podana przez użytkownika jest równa wylosowanej, wyświetlić ko
 
 package pl.coderslab;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        final int min = 1;
+        final int max = 10;
+    //generator liczb losowych
+        Random r = new Random();
+        int intDrawn = r.nextInt(max - min)+ 1;// ustawienie osi zeby liczyl od 1
 
+
+//        Random rand = new Random();
+//        int intDrawn = rand.nextInt(max)+min;
+        System.out.println("Wylosowana liczba: " + intDrawn);
+
+        Scanner scan = new Scanner(System.in);
+        int intUser = 0;
+        int noOfDraws = 0;
+        String prompt = "Podaj numer (w przedziale od " + min + " do " + max + "): ";
+        do {
+            noOfDraws ++;
+            System.out.println();
+            intUser = readIntRangeMinMax(prompt, min, max, scan);
+            if (intUser == intDrawn) {
+                System.out.println("Trafiłeś " + noOfDraws + " " + "razem " + "Zgadłeś:)\n Jesteś super :)");
+            } else if (intUser < intDrawn) {
+                System.out.println("Zbyt mała wartość: " + noOfDraws);
+            } else {
+                System.out.println("Zbyt duża wartość: " + noOfDraws);
+            }
+        } while (intUser != intDrawn);
+
+    }
+//Sprawdzenie czy dana liczba znajduje sie w  final int max.
+    static int readIntRangeMinMax(String prompt, int min, int max, Scanner scan) {
+        int inInt = 0;
+        do {
+            System.out.print(prompt);
+            while (!scan.hasNextInt()) {
+                System.out.println("Niepoprawne dane: ");
+                scan.next();
+                System.out.print(prompt);
+            }
+            inInt = scan.nextInt();
+            if ((inInt < min) || (inInt > max)) {
+                System.out.println("Niepoprawne dane: !!! podaj liczbę od 1 do 10");
+            }
+        } while ((inInt < min) || (inInt > max));
+        return inInt;
     }
 }
